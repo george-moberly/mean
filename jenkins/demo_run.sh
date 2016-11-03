7ed2975e0b6d72d5dc618f4dde8f27e044b259f7
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-11-03 16:56:15 -0700

    copy token

--- jenkins/demo_run.sh
+++ jenkins/demo_run.sh
@@ -120,7 +120,7 @@ do
   echo $w
   scp -o StrictHostKeyChecking=no -i /opt/ch/key.pem mongodb-org-3.2.repo ec2-user\@$w:/tmp
   ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'mkdir /opt/ch'"
-  ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'chmod 777 /opt/ch'
+  ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'chmod 777 /opt/ch'"
   scp -o StrictHostKeyChecking=no -i /opt/ch/key.pem /opt/ch/ch_token.txt ec2-user\@$w:/opt/ch
   ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'cp /tmp/mongodb-org-3.2.repo /etc/yum.repos.d/'"
   ssh -i /opt/ch/key.pem ec2-user\@$w "ls /etc/yum.repos.d/"
