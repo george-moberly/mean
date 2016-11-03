500afb00136da4fe04390995d6b857b130b9aeba
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-11-02 17:18:42 -0700

    add jenkins files

---
+++ jenkins/demo_run.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -x
+
+# copy latest templates to S3 (the mongo one is too big to work as a local file)
+#
+aws s3 cp MongoDB-VPC.template s3://test-gjm/MongoDB-VPC.template
+aws s3 cp VPC_AutoScaling_and_ElasticLoadBalancer.template s3://test-gjm/VPC_AutoScaling_and_ElasticLoadBalancer.template
+
+# run the mongo cluster (includes a VPN)
+#
+aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name MongoCluster --template-url http://s3.amazonaws.com/test-gjm/MongoDB-VPC.template --parameters ParameterKey=AvailabilityZone0,ParameterValue=us-east-1a ParameterKey=AvailabilityZone1,ParameterValue=us-east-1c ParameterKey=AvailabilityZone2,ParameterValue=us-east-1d ParameterKey=ClusterReplicaSetCount,ParameterValue=3 ParameterKey=ClusterShardCount,ParameterValue=1 ParameterKey=KeyName,ParameterValue=george ParameterKey=NodeInstanceType,ParameterValue=m3.medium ParameterKey=RemoteAccessCIDR,ParameterValue=0.0.0.0/0 ParameterKey=ShardsPerNode,ParameterValue=0 ParameterKey=VolumeSize,ParameterValue=16
+
+#-> example output
+# {
+#    "StackId": "arn:aws:cloudformation:us-east-1:530342348278:stack/MongoCluster/b282de70-9f2b-11e6-9c86-500c5240582a"
+#}
+
+# get the instance IP's out of the mongo side
+# the VPC and public subnet id's are written out to files as a side effect of running this (consumed by the web stuff)
+#
+
+aws cloudformation wait stack-create-complete --stack-name MongoCluster
+
+aws cloudformation list-stack-resources --stack-name MongoCluster | tee mongo_resources.json | perl -f get_instances.pl | tee mongo_instances.txt
+
+# these are the subnets in use
+#
+#VPC CIDR: 10.0.0.0/16
+#SecondaryReplicaSubnet0: 10.0.3.0/24
+#SecondaryReplicaSubnet1: 10.0.4.0/24
+#PrimaryReplicaSubnet: 10.0.2.0/24
+#PublicSubnet: 10.0.1.0/24
+
+# add the ASG, ELB, and web instnances into the public subnet
+#
+aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name WebCluster --template-url http://s3.amazonaws.com/test-gjm/VPC_AutoScaling_and_ElasticLoadBalancer.template --parameters ParameterKey=AZs,ParameterValue=us-east-1a ParameterKey=InstanceCount,ParameterValue=2 ParameterKey=InstanceType,ParameterValue=t2.large ParameterKey=KeyName,ParameterValue=george ParameterKey=Subnets,ParameterValue=`cat subnet.txt` ParameterKey=VpcId,ParameterValue=`cat vpc.txt`
+
+# stack-update-complete ??
+aws cloudformation wait stack-create-complete --stack-name WebCluster
+
+#-> example output
+# {
+#    "StackId": "arn:aws:cloudformation:us-east-1:530342348278:stack/WebCluster/2084c760-9f33-11e6-8aa1-50d5ca632656"
+#}
+
+aws cloudformation list-stack-resources --stack-name WebCluster | tee web_resources.json | perl -f get_instances.pl | tee web_instances.txt
+
+rm -f subnet.txt
+rm -f vpc.txt
+
+# steps to configure the web servers
+# install mongo client
+
+# doc sez but looks like CF template takes care of this -> The default /etc/mongod.conf configuration file supplied by the packages have bind_ip set to 127.0.0.1 by default. Modify this setting as needed for your environment before initializing a replica set.
+
+# get the primary mongo node
+export MONGO_PRIMARY=`cat mongo_instances.txt | grep PrimaryReplicaNode00NodeInstanceGP2 | awk '{print $NF}'`
+
+# all this needs to move to the webcluster CF template...
+#
+for w in `cat web_instances.txt | egrep "^WebServerGroup" | awk '{print $NF}'`
+do
+  echo $w
+  scp -o StrictHostKeyChecking=no -i /opt/ch/key.pem mongodb-org-3.2.repo ec2-user\@$w:/tmp
+  ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'cp /tmp/mongodb-org-3.2.repo /etc/yum.repos.d/'"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "ls /etc/yum.repos.d/"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'yum -y install mongodb-org-shell'"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "mongo $MONGO_PRIMARY:27017/test --eval 'printjson(db.getCollectionNames())'"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "nvm install v6.9.1"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "sudo su - root -c 'yum -y install git'"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "npm install -g bower"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "cd /home/ec2-user & git clone https://github.com/george-moberly/mean.git"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "cd /home/ec2-user/mean ; npm install"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "cd /home/ec2-user/mean ; npm install -g gulp"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "cd /home/ec2-user/mean ; npm install gulp"
+  ssh -i /opt/ch/key.pem ec2-user\@$w "export MONGOHQ_URL=mongodb://$MONGO_PRIMARY ; cd /home/ec2-user/mean ; npm start > /home/ec2-user/webapp.log 2>&1 &"
+done
+
+# when put operations are in API will publish the IP addresses
+#
+#curl -i https://api.confighub.com/rest/push \
+#     -H "Client-Token: `cat ~/ch_token.txt`"             \
+#     -H "Context: SalesDemos;TEST;MEAN-AWS;${MONGO_CLUSTER_CF_ID} "                \
+#     -H "Application-Name: MEAN"           \
+#     -H "Client-Version: v1.5"  
+#     -H "Files: demo.props" \
+#	-H "Value: $CF"
+
---
+++ jenkins/get_instances.pl
@@ -0,0 +1,142 @@
+
+while (<>) {
+	#print;
+	if (/AWS::EC2::Instance/) {
+		$iid = <>;
+		chomp($iid);
+		# "PhysicalResourceId": "i-0f5e95bdf28081eec", 
+		$iid =~ s/.*\".+\"\: \"//;
+		$iid =~ s/\"\,.*//;
+		#print "iid: $iid\n";
+		$foo = <>;
+		$foo = <>;
+		$nid = <>;
+		chomp($nid);
+		$nid =~ s/.*\".+\"\: \"//;
+		$nid =~ s/\".*//;
+
+		$private_ip = "";
+		$public_ip = "";
+
+		open(INSTANCE, "aws ec2 describe-instances --instance-ids $iid |");
+		while(<INSTANCE>) {
+			#print;
+			if (/.*PrivateIpAddress\"\: \"(.+)\"/) {
+				$private_ip = $1;
+			}
+			if (/.*PublicIpAddress\"\: \"(.+)\"/) {
+				$public_ip = $1;
+				print "scp -o StrictHostKeyChecking=no -i /opt/ch/key.pem ~/key.pem ec2-user\@${public_ip}:/tmp/\n"
+			}
+		}
+		print "$nid: $iid $private_ip $public_ip\n";
+	}
+	if (/AWS::EC2::Subnet\"/) {
+		#print;
+		$iid = <>;
+		chomp($iid);
+		# "PhysicalResourceId": "i-0f5e95bdf28081eec", 
+		$iid =~ s/.*\".+\"\: \"//;
+		$iid =~ s/\"\,.*//;
+		#print "iid: $iid\n";
+		$foo = <>;
+		$foo = <>;
+		$nid = <>;
+		chomp($nid);
+		$nid =~ s/.*\".+\"\: \"//;
+		$nid =~ s/\".*//;
+		#print "$iid: $nid\n";
+		if ($nid eq "DMZSubnet") {
+			print "subnet for WebCluster is: $iid\n";
+			open(SUBNET, ">subnet.txt");
+			print(SUBNET "$iid");
+			close(SUBNET);
+		}
+	}
+	if (/AWS::EC2::VPC\"/) {
+		#print;
+		$iid = <>;
+		chomp($iid);
+		# "PhysicalResourceId": "i-0f5e95bdf28081eec", 
+		$iid =~ s/.*\".+\"\: \"//;
+		$iid =~ s/\"\,.*//;
+		#print "iid: $iid\n";
+		$foo = <>;
+		$foo = <>;
+		$nid = <>;
+		chomp($nid);
+		$nid =~ s/.*\".+\"\: \"//;
+		$nid =~ s/\".*//;
+		#print "$iid: $nid\n";
+		print "VPC for WebCluster is: $iid\n";
+		open(VPC, ">vpc.txt");
+		print(VPC "$iid");
+		close(VPC);
+	}
+	if (/AWS::AutoScaling::AutoScalingGroup/) {
+		$iid = <>;
+		chomp($iid);
+		# "PhysicalResourceId": "i-0f5e95bdf28081eec", 
+		$iid =~ s/.*\".+\"\: \"//;
+		$iid =~ s/\"\,.*//;
+		print "ASG: $iid\n";		
+		$foo = <>;
+		$foo = <>;
+		$nid = <>;
+		chomp($nid);
+		$nid =~ s/.*\".+\"\: \"//;
+		$nid =~ s/\".*//;
+		print "nid: $nid\n";
+
+		open(ASG, "aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $iid |");
+		while(<ASG>) {
+
+			$private_ip = "";
+			$public_ip = "";
+			#print;
+			if (/.*InstanceId\"\: \"(.+)\"/) {
+				$instance_id = $1;
+				print "instance_id: $instance_id\n";
+				open(INSTANCE, "aws ec2 describe-instances --instance-ids $instance_id |");
+				while(<INSTANCE>) {
+					#print;
+					if (/.*PrivateIpAddress\"\: \"(.+)\"/) {
+						$private_ip = $1;
+					}
+					if (/.*PublicIpAddress\"\: \"(.+)\"/) {
+						$public_ip = $1;
+					}
+				}
+				print "$nid: $iid $private_ip $public_ip\n";
+			}
+		}
+	}
+	if (/AWS::ElasticLoadBalancing::LoadBalancer\"/) {
+		#print;
+		$iid = <>;
+		chomp($iid);
+		# "PhysicalResourceId": "i-0f5e95bdf28081eec", 
+		$iid =~ s/.*\".+\"\: \"//;
+		$iid =~ s/\"\,.*//;
+		#print "iid: $iid\n";
+		$foo = <>;
+		$foo = <>;
+		$nid = <>;
+		chomp($nid);
+		$nid =~ s/.*\".+\"\: \"//;
+		$nid =~ s/\".*//;
+		#print "$iid: $nid\n";
+		print "LoadBalancer iid:$iid nid:$nid\n";
+		#open(VPC, ">vpc.txt");
+		#print(VPC "$iid");
+		#close(VPC);
+		open(ELB, "aws elb describe-load-balancers --load-balancer-names $iid |");
+		while(<ELB>) {
+			# "DNSName": "my-load-balancer-1234567890.us-west-2.elb.amazonaws.com"
+			if (/DNSName.+\"(.+)\"/) {
+				$dns = $1;
+				print "load balancer DNS: $dns\n";
+			}
+		}
+	}
+}
