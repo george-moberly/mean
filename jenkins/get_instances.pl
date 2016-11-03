a16f316720d0e4c221f5efc2941436a4ccc1f93d
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-11-03 10:57:17 -0700

    typo

--- jenkins/get_instances.pl
+++ jenkins/get_instances.pl
@@ -27,9 +27,9 @@ while (<>) {
 			if (/.*PublicIpAddress\"\: \"(.+)\"/) {
 				$public_ip = $1;
 				open(NAT_SCRIPT, ">nat_ssh.sh");
-				print(NAT_SCRIPT, "scp -o StrictHostKeyChecking=no -i /opt/ch/key.pem /opt/ch/key.pem ec2-user\@${public_ip}:/tmp/\n");
-				print(NAT_SCRIPT, "ssh -i /opt/ch/key.pem ec2-user\@${public_ip} cd /tmp; chmod 400 key.pem");
-				print(NAT_SCRIPT, "ssh -i /opt/ch/key.pem ec2-user\@${public_ip}");
+				print(NAT_SCRIPT "scp -o StrictHostKeyChecking=no -i /opt/ch/key.pem /opt/ch/key.pem ec2-user\@${public_ip}:/tmp/\n");
+				print(NAT_SCRIPT "ssh -i /opt/ch/key.pem ec2-user\@${public_ip} cd /tmp; chmod 400 key.pem");
+				print(NAT_SCRIPT "ssh -i /opt/ch/key.pem ec2-user\@${public_ip}");
 				close(NAT_SCRIPT);
 			}
 		}
